51ce59f1d93f438e45406bcf1f59dd6f27a685b3
Radwan <ammanjordan1997@gmail.com>
2020-08-10 02:09:23 +0300

    last update 09/August/2020

--- src/main/java/WebPagesControllers/PagesNavigations.java
+++ src/main/java/WebPagesControllers/PagesNavigations.java
@@ -19,6 +19,8 @@ public class PagesNavigations {
 
     public String goToAddNewGame()
     {
+
+        messageToPlayer="";
         return "/pages/add_new_game.xhtml?faces-redirect = true";
     }
 
